25ed963694b8bf16f1e7bc857021e1c31c528bcf
Matt Anderson <matt@tonkapark.com>
2011-11-13 01:08:42 -0600

    simplified wrapper. fully tested

--- lib/bigcartel/client.rb
+++ lib/bigcartel/client.rb
@@ -10,13 +10,11 @@ module BigCartel
     headers 'Content-Type' => 'application/json' 
 
 	
-    attr_reader :subdomain, :store, :url, :pages, :products
+    attr_reader :account, :store, :url
     
-    def initialize(subdomain)
-      @subdomain = subdomain
-      @url = "http://#{subdomain}.bigcartel.com"
-        #set_store #initialize store data
-      #nitialize_store
+    def initialize(account)
+      @account = account
+      @url = "http://#{account}.bigcartel.com"
     end
 	
 	
@@ -32,121 +30,37 @@ module BigCartel
       response.map { |c| Hashie::Mash.new(c)}
     end	
 	
-    ##############################################
-    ## STORE
+
     def store
-      @store ||= self.class.fetch("/#{@subdomain}/store.js")
-      @url = @store.url
-      @store.products = products
-      @store        
+      self.class.fetch("/#{@account}/store.js")              
     end
     
-    #~ def initialize_store
-     #~ self.store unless @store.is_a?(Hash)
-     #~ true
-    #~ end    
-    ##############################################
-    ## 
-      def products(opts={})
-        self.store unless @store.is_a?(Hash)
-          @products ||= self.class.list("/#{@subdomain}/products.js", opts)
 
-        @products.each do |p|
+    def products(opts={})
+      products = self.class.list("/#{@account}/products.js", opts)
+
+      products.each do |p|
         p.images = images_helper(p.images)
-        p.full_url = "#{@url}#{p.url}"
+        p.full_url = "#{@account}#{p.url}"
         p.has_default_option = has_default_option?(p.options)
         p.option = p.options.first
-        end
-        
-        @products	 
       end
       
-      def product(id)
-        return product_by_id(id) if id.is_a?(Integer)
-        return product_by_permalink(id) if id.is_a?(String)
-      end
-      
-      def product_by_id(product_id)
-       @products.each do |p|
-         return p if p.id == product_id
-       end	
-      end
+      products	 
+    end
 
-      def product_by_permalink(permalink)
-       @products.each do |p|
-         return p if p.permalink == permalink
-       end	
-      end
-	
-      def products_by_category(permalink)      
-        result = []
-        if @store.categories.size > 0
-          @products.each do |p|
-            cats = {}
-            if p.categories.size > 0 
-              cats = p.categories.collect {|c| c.permalink}          
-              result << p if cats.include?(permalink)
-            end
-          end      
-        end
-        result
-      end    
-      
-      def products_by_artist(permalink)      
-        result = []
-        if @store.artists.size > 0
-          @products.each do |p|
-            temp = {}
-            if p.artists.size > 0 
-              temp = p.artists.collect {|a| a.permalink}          
-              result << p if temp.include?(permalink)
-            end
-          end      
-        end
-        result
-      end      
-    ##############################################
-    ## CATEGORIES
-
-      def category(permalink)
-       @store.categories.each do |c|
-         return c if c.permalink == permalink
-       end
-      end
-      
-      def categories
-       @store.categories
-     end
-     
-    ##############################################
-    ## ARTISTS
-          
-      def artist(permalink)
-       @store.artists.each do |a|
-         return a if a.permalink == permalink
-       end
-      end
-      
-      def artists
-       initialize_store
-       @store.artists
-      end
-     
-    ##############################################
-    ## PAGES
-      def pages
-        initialize_store
-          @pages ||= @store.pages.map { |p| page(p.permalink) }	  
-      end
-      
-      def page(permalink=nil)
-        self.class.fetch(URI.encode("/#{@subdomain}/page/#{permalink}.js"))
-      end  
+    
+    def page(permalink=nil)
+      self.class.fetch(URI.encode("/#{@account}/page/#{permalink}.js"))
+    end  
       
     ##############################################
     ## HELPERS
   private
-
+      def store_url(account)
+        "http://#{account}.bigcartel.com"
+      end
+      
       def has_default_option?(options)
         names = {}
         if options.size <= 1
--- spec/bigcartel_spec.rb
+++ spec/bigcartel_spec.rb
@@ -8,7 +8,7 @@ describe BigCartel::Client do
       stub_request(:get, "api.bigcartel.com/park/store.js").
         to_return(:body=>fixture("store.json"), :headers => {:content_type => "application/json; charset=utf-8"})
         
-      stub_request(:get, "api.bigcartel.com/park/products.js?limit=100").
+      stub_request(:get, /api.bigcartel.com\/park\/products.js\?limit=\d+/).
         to_return(:body=>fixture("products.json"), :headers => {:content_type => "application/json; charset=utf-8"})        
     end
       
@@ -43,7 +43,7 @@ describe BigCartel::Client do
       stub_request(:get, "api.bigcartel.com/park/store.js").
         to_return(:body=>fixture("store.json"), :headers => {:content_type => "application/json; charset=utf-8"})
         
-      stub_request(:get, "api.bigcartel.com/park/products.js?limit=100").
+      stub_request(:get, /api.bigcartel.com\/park\/products.js\?limit=\d+/).
         to_return(:body=>fixture("products.json"), :headers => {:content_type => "application/json; charset=utf-8"})        
     end
     
@@ -52,19 +52,10 @@ describe BigCartel::Client do
       a_request(:get, "api.bigcartel.com/park/store.js").
             should have_been_made   
             
-      a_request(:get, "api.bigcartel.com/park/products.js?limit=100").
-            should have_been_made          
+      a_request(:get, /api.bigcartel.com\/park\/products.js/).
+            should_not have_been_made          
     end     
   
-    it "will use stored hash instead of http calls" do 
-      store = @client.store        
-      a_request(:get, "api.bigcartel.com/park/store.js").
-            should_not have_been_made   
-            
-      a_request(:get, "api.bigcartel.com/park/products.js?limit=100").
-            should_not have_been_made  
-    end            
-    
     context "will be a valid hash" do
       before{@store = @client.store}
       
@@ -83,11 +74,120 @@ describe BigCartel::Client do
       it { @store.products_count.should be_an Integer  }
       it { @store.country.should be_an Hash  }
       it { @store.currency.should be_an Hash  }
-      it { @store.pages.should be_an Array  }      
-      it { @store.products.should be_an Array }
+      it { @store.pages.should be_an Array  }            
     end
   end
   
+  
+  #########################################
+  # product methods
+  
+  describe ".products" do
+    before do
+      @client = BigCartel::Client.new("park")
+      
+      stub_request(:get, "api.bigcartel.com/park/store.js").
+        to_return(:body=>fixture("store.json"), :headers => {:content_type => "application/json; charset=utf-8"})
+        
+      stub_request(:get, /api.bigcartel.com\/park\/products.js\?limit=\d+/).      
+        to_return(:body=>fixture("products.json"), :headers => {:content_type => "application/json; charset=utf-8"})        
+
+    end
+      
+    it "will make http api calls" do
+      products = @client.products
+      a_request(:get, "api.bigcartel.com/park/store.js").
+            should_not have_been_made   
+            
+      a_request(:get, "api.bigcartel.com/park/products.js?limit=100").
+            should have_been_made          
+    end     
+
+    it "accepts limit parameter" do
+      products = @client.products({:limit => 10}  )
+            
+      a_request(:get, "api.bigcartel.com/park/products.js?limit=10").
+            should have_been_made          
+    end    
+      
+    context "will be a valid hash" do
+      before{@product = @client.products.first}
+      
+      it {@product.should be_a Hash}
+      
+      it "should have a permalink" do        
+        @product.permalink.should be_an String
+        @product.permalink.should eq("watch-test")
+      end  
+        
+      it { @product.tax.should be_an Float  }
+      it { @product.created_at.should be_a String  }
+      it { @product.shipping.should be_an Array  }
+      it { @product.status.should be_a String  }      
+      it { @product.artists.should be_an Array  }
+      it { @product.categories.should be_an Array  }
+      it { @product.position.should be_an Integer  }
+      it { @product.images.should be_an Array  }
+      it { @product.images.first.thumb.should be_a String  }
+      it { @product.images.first.medium.should be_a String  }
+      it { @product.images.first.large.should be_a String  }
+      
+      it { @product.on_sale.should eq(!!@product.on_sale) }  #Boolean test
+      it { @product.description.should be_a String  }      
+      it { @product.price.should be_an Float }      
+      it { @product.name.should be_a String }      
+      it { @product.url.should be_a String }      
+      it { @product.default_price.should be_a Float }      
+      it { @product.id.should be_an Integer }      
+      it { @product.options.should be_an Array }     
+         
+    end      
+      
+  end
+  
+##################
+  describe ".page" do
+    before do
+      @client = BigCartel::Client.new("park")
+      
+      stub_request(:get, "api.bigcartel.com/park/store.js").
+        to_return(:body=>fixture("store.json"), :headers => {:content_type => "application/json; charset=utf-8"})
+        
+      stub_request(:get, /api.bigcartel.com\/park\/products.js/).      
+        to_return(:body=>fixture("products.json"), :headers => {:content_type => "application/json; charset=utf-8"})        
+
+       stub_request(:get, "api.bigcartel.com/park/page/about.js").
+        to_return(:body=>fixture("page.json"), :headers => {:content_type => "application/json; charset=utf-8"})
+   end
+
+   it "will make http api calls" do     
+     page = @client.page("about")
+     a_request(:get, "api.bigcartel.com/park/page/about.js").
+            should have_been_made
+            
+      a_request(:get, "api.bigcartel.com/park/store.js").
+            should_not have_been_made   
+            
+      a_request(:get, "api.bigcartel.com/park/products.js?limit=100").
+            should_not have_been_made   
+   end   
+          
+    describe 'should build hash' do
+      before(:each) { @page = @client.page("about")}
+      
+      it "should have a permalink" do                
+        @page.permalink.should be_a String
+        @page.permalink.should eq("about")
+      end  
+      
+      it { @page.content.should be_a String }
+      it { @page.category.should be_a String  }
+      it { @page.name.should be_a String  }
+      it { @page.url.should be_a String  }
+      it { @page.id.should be_an Integer  }
+    end    
+    
+  end
   
   
 end
